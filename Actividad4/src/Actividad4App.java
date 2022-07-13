e34ca03138b77c9c3e3e7665ad11ab17f9a8a9bc
FelipeGomezGarcia <feli.goga@gmail.com>
2022-07-13 12:54:48 +0200

    Actividad 4

---
+++ Actividad4/src/Actividad4App.java
@@ -0,0 +1,13 @@
+
+public class Actividad4App {
+	public static void main (String[] args) {
+		
+		Serie s1 = new Serie("La que se avecina", 10, "Comedia", "Telecinco");
+		
+		System.out.println(s1);
+		
+		Serie s2 = new Serie("La resistencia", "Movistar");
+		
+		System.out.println(s2);
+	}
+}
