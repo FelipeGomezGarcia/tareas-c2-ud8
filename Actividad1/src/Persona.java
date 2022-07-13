75c7931075f3a4b88ab76ed22a8d55ef00bda37d
FelipeGomezGarcia <feli.goga@gmail.com>
2022-07-13 11:03:48 +0200

    Actividad 1

---
+++ Actividad1/src/Persona.java
@@ -0,0 +1,39 @@
+
+public class Persona {
+	private final char GENERO = 'M';
+	
+	private String nombre;
+	private int edad;
+	private String dni;
+	private char sexo;
+	private double peso;
+	private double altura;
+	
+	public Persona(String dni) {
+		this.nombre = "";
+		this.edad = 0;
+		this.dni = dni;
+		this.sexo = GENERO;
+		this.peso = 0.0;
+		this.altura = 0.0;
+	}
+
+	public Persona(String nombre, int edad, String dni, char sexo) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dni = dni;
+		this.sexo = sexo;
+		this.peso = 0.0;
+		this.altura = 0.0;
+	}
+
+	public Persona(String nombre, int edad, String dni, char sexo, double peso, double altura) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dni = dni;
+		this.sexo = sexo;
+		this.peso = peso;
+		this.altura = altura;
+	}
+	
+}
